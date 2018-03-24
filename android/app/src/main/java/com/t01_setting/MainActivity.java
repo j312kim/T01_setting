1910405303c13c3093244331cc7c9964abe26a49
j312kim <kimjinho312@naver.com>
2018-03-24 13:24:30 +0900

    initial commit

---
+++ android/app/src/main/java/com/t01_setting/MainActivity.java
@@ -0,0 +1,15 @@
+package com.t01_setting;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "T01_setting";
+    }
+}
